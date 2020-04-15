c5b36bbb2f8df51866239c249a5efc2bf5f2e750
Andrew Insignares <andrewinsignares@gmail.com>
2020-04-15 15:13:51 -0400

    booty

--- finger.py
+++ finger.py
@@ -9,19 +9,20 @@ import os
 
 
 
+#access directory for json
 
-os.chdir(r'C:\Users\jw887\Pictures') #access directory for json
+os.chdir(r'C:\Users\jw887\Pictures') 
 
 
+#open up that config
 
-
-with open('config-sean.json') as config_file: #open up that config
+with open('config-sean.json') as config_file: 
 	data = json.load(config_file)
 
 
+#here are the ingredients we're looking for from the json file
 
-
-organization_id = data['organization_id'] #here are the ingredients we're looking for from the json file
+organization_id = data['organization_id']
 key_code = data['key_code']
 secret_key_code = data['secret_key_code']
 
@@ -38,19 +39,21 @@ print(secret_key_code)
 print ("X-Time")
 
 
-
+#this function provides an active time stamp wheneverthe code is run
+#here we convert the function's result into a string
 
 def generate_time():
-    return int(round(time.time() * 1000))#this function provides an active time stamp wheneverthe code is run
-xTime = str(generate_time()) #here we convert the function's result into a string
+    return int(round(time.time() * 1000))
+xTime = str(generate_time()) 
 print (xTime)
 
 
-
+#this function provides 36 digit string of random numbers
+#here we concatanate the smaller pieces
 
 print ("X-Nonce")
-def generate_nonce(length=36): #this function provides 36 digit string of random numbers
-    return ''.join([str(random.randint(0, 9)) for i in range(length)]) #here we concatanate the smaller pieces
+def generate_nonce(length=36): 
+    return ''.join([str(random.randint(0, 9)) for i in range(length)]) 
 xNonce = generate_nonce()
 print (xNonce)
 
@@ -63,52 +66,35 @@ print ("X-Request-Id")
 
 
 
+#this function provides a universally unique identifier
 
-print (uuid.uuid4()) #this function provides a universally unique identifier
+print (uuid.uuid4()) 
 
 
+#these are instructions for our API request and how it's going to access Nicehash
 
+
+#this is a setup for hashing the instructions for our apirequest
 
 print ("X-Auth")
-requestid= 'GET' #these are instructions for our API request and how it's going to access Nicehash
-requestpath= '/main/api/v2/mining/rigs2/' #this is a setup for hashing the instructions for our apirequest
+requestid= 'GET'
+requestpath= '/main/api/v2/mining/rigs2/' 
 
 
 
+def hashgen():
+    hash1 = bytes(key_code, 'utf-8')
+    hash2 = bytes(xTime, 'utf-8')
+    h = hmac.new(hash1, hash2, digestmod=hashlib.sha256)    
+    h.update(bytes(xNonce, 'utf-8'));
+    h.update(bytes(organization_id, 'utf-8'));
+    h.update(bytes(requestid,'utf-8'));
+    h.update(bytes(requestpath,'utf-8'));
+    #h.update(bytes(digestmod, 'utf-8'))
 
-#def hashgen():#here's our grand hmac hash function. we're still figuring out how to compile everything 
-hash1 = bytes(key_code, 'utf-8')
-hash2 = bytes(xTime, 'utf-8')
- #   h = hmac.new(hash1, hash2, digestmod=hashlib.sha256)
- #   return(h.digest())
-
-
-
-
-#def hashgen2():
-hash3 = bytes(xNonce, 'utf-8')
-hash4 = bytes(organization_id, 'utf-8')
-  #  h2 = hmac.new(hash3, hash4, digestmod=hashlib.sha256)
-   # return(h2.digest())
+    return(h.digest())
 
 
-
-
-#def hashgen3():
-hash5 = bytes(requestid,'utf-8')
-hash6 = bytes(requestpath,'utf-8')
-   # h3 = hmac.new(hash5, hash6, digestmod=hashlib.sha256)
-   # return(h3.digest())
-
-#hash7 = bytes(digestmod, 'utf-8')
-
-
-def hashgen4():
-    
-    hashx = hash1, hash2, hash3, hash4
-    hashy = hash5, hash6
-    h4 = hmac.new(hashx, hashy, digestmod=hashlib.sha256)
-    return(h4.digest())
 
 
 
@@ -125,4 +111,4 @@ def hashgen4():
  #  finalHash = finalHash + ''
  #   finalHash = finalHash + ''    
 #    return(finalHash)
-print (hashgen4())
+print (hashgen())
