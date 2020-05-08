18c196bdff8fd7f2009010bef60383746082ab99
Andrew Insignares <andrewinsignares@gmail.com>
2020-05-08 16:10:16 -0400

    some commit message

--- Main_API_Request.py
+++ Main_API_Request.py
@@ -6,22 +6,20 @@ import base64
 import random
 import time
 import os
+import requests
 
 
 
 #access directory for json
-
 os.chdir(r'C:\Users\jw887\Pictures') 
 
-
 #open up that config
-
 with open('config-sean.json') as config_file: 
 	data = json.load(config_file)
 
 
-#here are the ingredients we're looking for from the json file
 
+#here are the ingredients we're looking for from the json file
 organization_id = data['organization_id']
 key_code = data['key_code']
 secret_key_code = data['secret_key_code']
@@ -29,56 +27,29 @@ secret_key_code = data['secret_key_code']
 
 
 
-print(organization_id)
-print(key_code)
-print(secret_key_code)
-      
-
-
-
-print ("X-Time")
-
-
 #this function provides an active time stamp wheneverthe code is run
 #here we convert the function's result into a string
-
 def generate_time():
+
     return int(round(time.time() * 1000))
+
 xTime = str(generate_time()) 
-print (xTime)
+
+
 
 
 #this function provides 36 digit string of random numbers
 #here we concatanate the smaller pieces
-
-print ("X-Nonce")
 def generate_nonce(length=36): 
+
     return ''.join([str(random.randint(0, 9)) for i in range(length)]) 
+
 xNonce = generate_nonce()
-print (xNonce)
 
 
 
-
-print ("X-Organization-Id")
-print (organization_id)
-print ("X-Request-Id")      
-
-
-
-#this function provides a universally unique identifier
-
-print (uuid.uuid4()) 
-
-
-#these are instructions for our API request and how it's going to access Nicehash
-
-
-#this is a setup for hashing the instructions for our apirequest
-
-print ("X-Auth")
-requestid= 'GET'
-requestpath= '/main/api/v2/mining/rigs2/' 
+#requestid=
+requestpath= "https://api2.nicehash.com/main/api/v2/mining/algo/stats" 
 
 
 
@@ -92,11 +63,26 @@ def hashgen():
     h.update(bytes(requestpath,'utf-8'));
     #h.update(bytes(digestmod, 'utf-8'))
 
+
+
     return(h.digest())
 
 
 
 
+print(organization_id)
+print(key_code)
+print(secret_key_code)
+print ("X-Time")
+print (xTime)
+print ("X-Nonce")
+print (xNonce)
+print ("X-Organization-Id")
+print (organization_id)
+print ("X-Request-Id")      
+print (uuid.uuid4())
+print ("X-Auth")
+print (hashgen())
 
 
     #finalHash = ''
@@ -111,4 +97,4 @@ def hashgen():
  #  finalHash = finalHash + ''
  #   finalHash = finalHash + ''    
 #    return(finalHash)
-print (hashgen())
+
