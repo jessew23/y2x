c6ccd022fd6386a661ddc4dec90df518deae84b2
Andrew Insignares <andrewinsignares@gmail.com>
2020-05-08 17:04:54 -0400

    xd

--- Main_API_Request.py
+++ Main_API_Request.py
@@ -6,21 +6,33 @@ import base64
 import random
 import time
 import os
+
+
+
 #######################################################
+#
 # Execution Code
+
 #access directory for json
 os.chdir(r'C:\Users\jw887\Pictures')
+
 #open up that config
 with open('config-sean.json') as config_file:
     data = json.load(config_file)
+
 organization_id = data['organization_id']
 key_code = data['key_code']
 secret_key_code = data['secret_key_code']
 xTime = str(generate_time())
 xNonce = generate_nonce()
 requestpath= "https://api2.nicehash.com/main/api/v2/mining/algo/stats"
+
+
+
 #######################################################
+#
 # Result
+
 print(organization_id)
 print(key_code)
 print(secret_key_code)
@@ -35,15 +47,20 @@ print (uuid.uuid4())
 print ("X-Auth")
 print (hashgen())
 #######################################################
+#
 # Functions
+
+
 #this function provides an active time stamp wheneverthe code is run
 #here we convert the function's result into a string
 def generate_time():
     return int(round(time.time() * 1000))
+
 #this function provides 36 digit string of random numbers
 #here we concatanate the smaller pieces
 def generate_nonce(length=36):
     return ''.join([str(random.randint(0, 9)) for i in range(length)])
+
 def hashgen():
     hash1 = bytes(key_code, 'utf-8')
     hash2 = bytes(xTime, 'utf-8')
@@ -53,4 +70,5 @@ def hashgen():
     h.update(bytes(requestid,'utf-8'));
     h.update(bytes(requestpath,'utf-8'));
     #h.update(bytes(digestmod, 'utf-8'))
-    return(h.digest())
+
+   return(h.digest())
