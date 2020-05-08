2b330497444049f85d75c86c6399e84f4d8776c0
Andrew Insignares <andrewinsignares@gmail.com>
2020-05-08 16:39:20 -0400

    xd

--- Main_API_Request.py
+++ Main_API_Request.py
@@ -8,6 +8,14 @@ import time
 import os
 import requests
 
+organization_id = data['organization_id']
+key_code = data['key_code']
+secret_key_code = data['secret_key_code']
+xTime = str(generate_time()) 
+xNonce = generate_nonce()
+#requestid=
+requestpath= "https://api2.nicehash.com/main/api/v2/mining/algo/stats" 
+
 
 
 #access directory for json
@@ -17,14 +25,7 @@ os.chdir(r'C:\Users\jw887\Pictures')
 with open('config-sean.json') as config_file: 
 	data = json.load(config_file)
 
-
-
 #here are the ingredients we're looking for from the json file
-organization_id = data['organization_id']
-key_code = data['key_code']
-secret_key_code = data['secret_key_code']
-
-
 
 
 #this function provides an active time stamp wheneverthe code is run
@@ -33,24 +34,11 @@ def generate_time():
 
     return int(round(time.time() * 1000))
 
-xTime = str(generate_time()) 
-
-
-
-
 #this function provides 36 digit string of random numbers
 #here we concatanate the smaller pieces
 def generate_nonce(length=36): 
 
     return ''.join([str(random.randint(0, 9)) for i in range(length)]) 
-
-xNonce = generate_nonce()
-
-
-
-#requestid=
-requestpath= "https://api2.nicehash.com/main/api/v2/mining/algo/stats" 
-
 
 
 def hashgen():
@@ -63,11 +51,7 @@ def hashgen():
     h.update(bytes(requestpath,'utf-8'));
     #h.update(bytes(digestmod, 'utf-8'))
 
-
-
     return(h.digest())
-
-
 
 
 print(organization_id)
